40155bdb461e94e55f5bb8e801686c2aef6d366e
akhilgite <akhilgite@gmail.com>
2019-05-01 15:56:36 +0530

    init

---
+++ app/src/main/java/com/akhil/musicplayerdashboard/MainActivity.java
@@ -0,0 +1,19 @@
+package com.akhil.musicplayerdashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
